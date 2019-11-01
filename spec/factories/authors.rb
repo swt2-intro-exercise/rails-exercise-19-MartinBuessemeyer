6a54219f22e11e351f3c80c42deb49d1c7dcf439
Martin Buessemeyer <Martin.Buessemeyer@student.hpi.de>
2019-11-01 23:20:39 +0100

    finished issue 3

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
-  factory :author do
-    # first_name { "Alan" }
-    # last_name { "Turing" }
-    # homepage { "http://wikipedia.de/Alan_Turing" }
+  def name
+    first_name.concat(" ", last_name)
   end
 end
